8d18168c7402cdef27b08bacee09273c3e5cc8d6
威 刘 <lw1140@163.com>
2022-05-11 14:25:03 +0800

    feat: 增加基本流程测试，等待后续调试

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
-while (!click('通讯录'));
-sleep(1000);
+'auto';
+auto();
 
-var v = id('f1c').findOne().bounds();
-// var v = id('f1c').findOne().bounds();
-click(v.centerX(), v.centerY());
-// sleep(1000);
-// var r = id("ipm").text("添加朋友").findOne().bounds();
-// //log(r);
-// click(r.centerX(), r.centerY());
-// sleep(1000);
+log(+new Date());
+
+// 打开添加朋友页面
+// const addBtn = id('gdh').findOne().bounds();
+// click(addBtn.centerX(), addBtn.centerY());
+
+// const addFriendsBtn = id('ipm').text('添加朋友').findOne().bounds();
+// click(addFriendsBtn.centerX(), addFriendsBtn.centerY());
+
+// 输入电话号
+// const searchBtn = id('hej').findOne().bounds();
+// click(searchBtn.centerX(), searchBtn.centerY());
+
+// plan B
+
+// 打开通讯录
+// const tongxunlu = id('dub')
+//   .className('android.widget.TextView')
+//   .text('通讯录')
+//   .findOne()
+//   .bounds();
+// click(tongxunlu.centerX(), tongxunlu.centerY());
+
+// 点击新的朋友
+// var newFriBtn = className('android.widget.TextView')
+//   .text('新的朋友')
+//   .findOne()
+//   .bounds();
+// click(newFriBtn.centerX(), newFriBtn.centerY());
+
+// 添加手机联系人
+// var friendOfTongXunLu = id('gp').findOne().bounds();
+// click(friendOfTongXunLu.centerX(), friendOfTongXunLu.centerY());
+
+// 添加
+// var tianjia = className('android.widget.TextView')
+//   .text('添加')
+//   .findOne()
+//   .bounds();
+// click(tianjia.centerX(), tianjia.centerY());
+
+// const btns = className('android.widget.TextView').text('添加').find();
+
+// let i = 0;
+
+// while (true) {
+//   if (i === btns.length) {
+//     toast('无法添加更多');
+//     console.log(i);
+//     break;
+//   }
+
+//   const rect = btns[i++].bounds();
+//   click(rect.centerX(), rect.centerY());
+//   sleep(5000);
+//   // 更改好友申请
+//   const textarea = id('h_j').className('android.widget.EditText').findOne();
+//   textarea.setText(+new Date());
+// }
+
+// 点击发送按钮
+// var sendBtn = id('d6').className('android.widget.Button').findOne().bounds();
+// click(sendBtn.centerX(), sendBtn.centerY());
+
+// 返回
+// back();
+// sleep(5000);
+// back();
